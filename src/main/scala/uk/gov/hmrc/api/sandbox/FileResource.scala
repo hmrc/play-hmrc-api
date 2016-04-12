e605989a4cc4fce4e99d8938d377bfba00691802
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-04-12 12:34:42 +0100

    add FileResource sandbox trait

---
+++ src/main/scala/uk/gov/hmrc/api/sandbox/FileResource.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.api.sandbox
+
+
+trait FileResource {
+
+  import java.io.InputStream
+
+  import play.api.Logger
+
+  import scala.io.Source
+
+  private def readStreamToString(is: InputStream) = {
+    try Source.fromInputStream(is).mkString.toString
+    finally is.close()
+  }
+
+  def findResource(path: String): Option[String] = {
+    val resource = getClass.getResourceAsStream(path)
+    if (resource == null) {
+      Logger.warn(s"Could not find resource '$path'")
+      None
+    } else {
+      Some(readStreamToString(resource))
+    }
+  }
+}
