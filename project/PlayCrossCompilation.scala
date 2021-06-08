4011d3c2173af3cbbd300b7919b0683a9ea81766
chotai <chotai@users.noreply.github.com>
2021-06-08 15:59:54 +0100

    BDOG-1268 Re-use Play 2.7 source for Play 2.8

--- project/PlayCrossCompilation.scala
+++ project/PlayCrossCompilation.scala
@@ -1,4 +1,12 @@
 import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
 import uk.gov.hmrc.playcrosscompilation.PlayVersion._
 
-object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play26)
+object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play26) {
+
+  override lazy val playDir: String = playVersion match {
+    case Play26 => "play-26"
+    case Play27 => "play-27"
+    case Play28 => "play-27"
+  }
+
+}
