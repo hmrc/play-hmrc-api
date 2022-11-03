8a71a763ad3d94ba8c9d5c6ceed226ebf052cc78
Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>
2022-11-03 11:32:33 +0000

    Upgrading to scala 2.13 and cross compiling with 2.12 + sbt upgrade
    Signed-off-by: Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>

--- build.sbt
+++ build.sbt
@@ -4,10 +4,11 @@ name := "play-hmrc-api"
 lazy val library = (project in file("."))
   .disablePlugins(JUnitXmlReportPlugin)
   .settings(
-    scalaVersion := "2.12.14",
+    scalaVersion := "2.13.8",
     majorVersion := 7,
     isPublicArtefact := true,
     libraryDependencies ++= LibraryDependencies(),
     resolvers += Resolver.jcenterRepo,
+    crossScalaVersions := Seq("2.12.14", "2.13.8"),
     PlayCrossCompilation.playCrossCompilationSettings
   )
--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -20,7 +20,7 @@ object LibraryDependencies {
         "org.mockito"            % "mockito-core"               % "3.8.0"             % Test
       ),
       play28 = Seq(
-        "uk.gov.hmrc"            %% "service-integration-test"  % "1.1.0-play-28"     % Test,
+        "uk.gov.hmrc"            %% "service-integration-test"  % "1.3.0-play-28"     % Test,
         "org.scalatestplus.play" %% "scalatestplus-play"        % "5.0.0"             % Test,
         "com.vladsch.flexmark"   %  "flexmark-all"              % "0.35.10"           % Test
       )
